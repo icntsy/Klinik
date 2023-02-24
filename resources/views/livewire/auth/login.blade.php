1012ba9725d42b75608eabb2dd08e40f1bccca20
icntsy <icanatasya72@gmail.com>
2023-02-24 14:12:43 +0700

    template

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -4,58 +4,114 @@
             <div class="mx-auto app-login-box col-md-8">
                 <div class="modal-dialog w-100 mx-auto">
                     <form class="" method="post" wire:submit.prevent="login">
-                    <div class="modal-content">
+                        <div class="modal-content">
 
-                        <div class="modal-body">
-                            <div class="h5 modal-title text-center">
-                                <h4 class="mt-2">
-                                    <img src="{{asset('assets/images/logo.png')}}" alt="">
-                                    <div class="mt-3">Selamat Datang</div>
-                                </h4>
-                            </div>
+                            <div class="modal-body">
+                                <div class="h5 modal-title text-center">
+                                    <h4 class="mt-2">
+                                        <img src="{{ asset('assets/images/logo.png') }}" alt="">
+                                        <div class="mt-3">Selamat Datang</div>
+                                    </h4>
+                                </div>
 
                                 <div class="form-row">
                                     <div class="col-md-12">
                                         <div class="position-relative form-group">
                                             <label for="exampleEmail" class="control-label">Alamat Email</label>
                                             <input wire:model="email" name="email" id="exampleEmail"
-                                                   placeholder="Masukan Email" type="email" autofocus
-                                                   class="form-control @error('email') is-invalid @enderror">
+                                                placeholder="Masukan Email" type="email" autofocus
+                                                class="form-control @error('email') is-invalid @enderror">
                                             @error('email')
-                                            <div class='invalid-feedback'>{{ $message }}</div>@enderror
+                                                <div class='invalid-feedback'>{{ $message }}</div>
+                                            @enderror
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="position-relative form-group">
                                             <label for="examplePassword" class="control-label">Password</label>
                                             <input wire:model="password" name="password" id="examplePassword"
-                                                   placeholder="Masukan Password" type="password"
-                                                   class="form-control @error('password') is-invalid @enderror">
+                                                placeholder="Masukan Password" type="password"
+                                                class="form-control @error('password') is-invalid @enderror">
                                             @error('password')
-                                            <div class='invalid-feedback'>{{ $message }}</div>@enderror
+                                                <div class='invalid-feedback'>{{ $message }}</div>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
                                 <div class="position-relative form-check">
                                     <input name="check" wire:model="keeplogin" id="exampleCheck" type="checkbox"
-                                           class="form-check-input">
+                                        class="form-check-input">
                                     <label for="exampleCheck" class="form-check-label">Keep me logged in</label>
                                 </div>
-                        </div>
-                        <div class="modal-footer clearfix">
-                            <div class="float-left">
-                                <a href="javascript:void(0);" class="btn-lg btn btn-link">Recover Password</a>
                             </div>
-                            <div class="float-right">
-                                <button type="submit" class="btn btn-primary btn-lg">Login to
-                                    Dashboard
-                                </button>
+                            <div class="modal-footer clearfix">
+                                <div class="float-left">
+                                    <a href="javascript:void(0);" class="btn-lg btn btn-link">Recover Password</a>
+                                </div>
+                                <div class="float-right">
+                                    <button type="submit" class="btn btn-primary btn-lg">Login to
+                                        Dashboard
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+
+
+
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth px-0">
+            <div class="row w-100 mx-0">
+                <div class="col-lg-4 mx-auto">
+                    <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                        <div class="brand-logo">
+                            <img src="../../images/logo.svg" alt="logo">
+                        </div>
+                        <h4>Hello! let's get started</h4>
+                        <h6 class="fw-light">Sign in to continue.</h6>
+                        <form class="pt-3">
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-lg" id="exampleInputEmail1"
+                                    placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-lg" id="exampleInputPassword1"
+                                    placeholder="Password">
+                            </div>
+                            <div class="mt-3">
+                                <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
+                                    href="../../index.html">SIGN IN</a>
+                            </div>
+                            <div class="my-2 d-flex justify-content-between align-items-center">
+                                <div class="form-check">
+                                    <label class="form-check-label text-muted">
+                                        <input type="checkbox" class="form-check-input">
+                                        Keep me signed in
+                                    </label>
+                                </div>
+                                <a href="#" class="auth-link text-black">Forgot password?</a>
+                            </div>
+                            <div class="mb-2">
+                                <button type="button" class="btn btn-block btn-facebook auth-form-btn">
+                                    <i class="ti-facebook me-2"></i>Connect using facebook
+                                </button>
+                            </div>
+                            <div class="text-center mt-4 fw-light">
+                                Don't have an account? <a href="register.html" class="text-primary">Create</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
 </div>
